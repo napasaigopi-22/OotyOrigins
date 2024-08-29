947882576d7bcd3e2109c5ec0c3cfc582bbd1960
Mahesh Narayanabhatla <cb.sc.p2cse24015@cb.students.amrita.edu>
2024-08-29 10:57:26 +0530

    changed password

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require("mongodb");
 
-const uri = "mongodb+srv://mahesh:maheshaa@cluster0.umn1m4x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://mahesh:<password>@cluster0.umn1m4x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const client = new MongoClient(uri);
 
