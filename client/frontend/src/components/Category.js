ac4bc443a6c4ae72cf294d4c626861d8cb7680e0
Mahesh Narayanabhatla <cb.sc.p2cse24015@cb.students.amrita.edu>
2024-10-07 18:36:21 +0530

    some category changes

--- client/frontend/src/components/Category.js
+++ client/frontend/src/components/Category.js
@@ -1,18 +1,66 @@
 import { useLocation } from "react-router-dom";
 import NavBar from "../Assets/NavBar";
+import { useEffect, useState } from "react";
+import Axios from "axios";
+import { Box, Grid2 } from "@mui/material";
+import Product from "../Assets/Product";
 
-function Category()
-{
+function Category() {
     const location = useLocation();
-  const value = location.state;
-  console.log(value.name)
+    var value = location.state;
+    console.log(value.name);
+    const [product, setproduct] = useState([]);
+    const [showProd, setshowProd] = useState([]);
+    const [loaded, setloaded] = useState(false);
+
+    useEffect(() => {
+        Axios.get('http://localhost:4000/get/products').then(res => {
+            setproduct(res.data);
+            console.log("product called ",product,"==setted prods==",res.data);
+            setshowProd(product.filter(ele=> ele.category == value.name));
+        }).catch(function (error) {
+            console.log(error);
+        });
+    },[loaded]);
+
+    useEffect(() => {
+        Axios.get('http://localhost:4000/get/products').then(res => {
+            setproduct(res.data);
+            console.log("product called ",product,"==setted prods==",res.data);
+            setshowProd(product.filter(ele=> ele.category == value.name));
+            setloaded(true);
+        }).catch(function (error) {
+            console.log(error);
+        });
+    }, [location.state]);
+
+
     return (
         <>
-        <NavBar></NavBar>
-        <div>
-            <h1>Category Page</h1>
-            <h5>{value.name}</h5>
-        </div>
+            <NavBar></NavBar>
+            <div>
+                <h1>Category Page</h1>
+                <h5>{value.name}</h5>
+
+                <Box sx={{ flexGrow: 1 }}>
+                    <Grid2
+                        container
+                        spacing={2}
+                        direction="row"
+                        justify="flex-start"
+                        alignItems="flex-start"
+                        style={{ margin: "auto", width: '90%' }}
+                    >
+                        {
+                            showProd.map((val, key) => {
+                                return <Grid2 item xs={4} size={4} >
+                                    <Product style={{ margin: 'auto' }} prdId={val.productId} name={val.name} cost={val.price} stock={val.stock}  ></Product>
+                                </Grid2>
+                            })
+                        }
+                    </Grid2>
+                </Box>
+            </div>
         </>
     )
 }
